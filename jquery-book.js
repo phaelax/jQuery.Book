04147d7afaad17ea8505270d1438d83ded7b32e8
phaelax <phaelax@gmail.com>
2020-10-26 15:47:11 -0400

    Update jquery-book.js

--- jquery-book.js
+++ jquery-book.js
@@ -9,12 +9,22 @@
 		
 		var settings = $.extend(defaults, options);
 		
-		var pageIndex = 0;
 		
-		var pages = $(this).children('section');
+		if (this.length > 1){
+			this.each(function(){ $(this).book(options) });
+			return this;
+		}
+		
+		var pageIndex = 0;
 		
 		var $this = $(this);
 		
+		var pages = $this.children('section').css({width:'100%',height:'100%',position:'relative'}); // The sections need to match the parent (<form>) container's size for animation to look correct
+		
+		
+		
+		// The form will expand to fit the container it's in (unless overridden).
+		//this.css({width:'100%', display:'flex', margin:'auto', overflow:'hidden'});  
 		
 		this.initialize = function(){
 			
@@ -24,6 +34,8 @@
 			pages.find('.page-next').on('click', this.nextPage);
 		
 			pages.find('.page-prev').on('click', this.prevPage);
+			
+			return this;
 		}
 		
 		
